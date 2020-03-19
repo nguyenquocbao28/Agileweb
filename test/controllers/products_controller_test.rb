e6c4236580f52e2bcede67bf619c9589ca4cc55f
nguyenquocbao28 <nguyenquocbao759@gmail.com>
2020-03-19 15:00:20 +0700

    Cart Creation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,17 +1,14 @@
-require 'test_helper'
+require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
     @update = {
-      title:
-      'Lorem Ipsum',
-      description: 'Wibbles are fun!',
-      image_url:
-      'lorem.jpg',
-      price:
-      19.95
-      }
+      title: "Lorem Ipsum",
+      description: "Wibbles are fun!",
+      image_url: "lorem.jpg",
+      price: 19.95,
+    }
   end
 
   test "should get index" do
@@ -25,8 +22,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create product" do
-    assert_difference('Product.count') do
-      post products_url, params: { product: @update}
+    assert_difference("Product.count") do
+      post products_url, params: { product: @update }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -47,8 +44,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to product_url
+  end
+
   test "should destroy product" do
-    assert_difference('Product.count', -1) do
+    assert_difference("Product.count", -1) do
       delete product_url(@product)
     end
 
